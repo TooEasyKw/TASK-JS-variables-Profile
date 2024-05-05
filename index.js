7550d112e54f8b22a0f80705f80159221f386237
Dawood <dawood.boland.kw@gmail.com>
2024-05-05 17:58:39 +0300

    all tasks done

--- index.js
+++ index.js
@@ -11,10 +11,12 @@
 
 // Part 1 answer 👇🏻 ...
 
-
-
-
-
+const fullName = "Dawood Boland";
+const yearOfBirth = 1999;
+const hobby = "Teaching";
+const funFact = "Sleeping is my favorite hobby";
+const image =
+  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/13e29d6d-ed2a-4e04-986e-3bcfab9169d1/dfygl1q-ef9ed28f-f91d-4e0c-a3e8-da02144e718a.jpg/v1/fill/w_894,h_894,q_70,strp/anime_profile_pic_toji_fushiguro_by_vaibhavverma2005_dfygl1q-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA4MCIsInBhdGgiOiJcL2ZcLzEzZTI5ZDZkLWVkMmEtNGUwNC05ODZlLTNiY2ZhYjkxNjlkMVwvZGZ5Z2wxcS1lZjllZDI4Zi1mOTFkLTRlMGMtYTNlOC1kYTAyMTQ0ZTcxOGEuanBnIiwid2lkdGgiOiI8PTEwODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.GVTEEa0hr8Zn8p3Wca8X7IQmSpJN4jWaPPNR8MH6mKs";
 
 /**
  * Part 2: String Interpolation
@@ -24,35 +26,35 @@
  * 1. fullNameString      -> assign it to: My name is {fullName}
  * 2. yearOfBirthString   -> assign it to: I am {YOUR_AGE}, and make sure you calculate your age from your year of birth
  * 3. hobbyString         -> assign it to: My hobby is {YOUR_HOBBY}
- * 
+ *
  * Uncomment the following lines and start interpolating.
  * You can uncomment a line by removing the // at the start
  */
 
-// const fullNameString = `My name is ${}`;
-// const yearOfBirthString =
-// const hobbyString =
+fullNameString = "My name is Dawood Boland";
+yearOfBirthString = "I am 25 years old";
+hobbyString = "My hobby is Eating Wagyu Steak";
 
-
-
-
-/**
- * Part 3: Re-assignment
- * Increment your hacker score
- * */
+//  * Part 3: Re-assignment
+//  * Increment your hacker score
+//  * */
 
 let stars = 0;
 
 function incrementBy1() {
   // Increment stars by 1 👇🏻
+  stars += 1;
 }
 function decrementBy1() {
   // decrement stars by 1 👇🏻
+  stars -= 1;
 }
 
 function incrementBy2() {
   // Increment stars by 2 👇🏻
+  stars += 2;
 }
 function decrementBy2() {
   // decrement stars by 2 👇🏻
+  stars -= 2;
 }
